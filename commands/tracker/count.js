ea9c57030cfdfe2a92ac4abbea4a4c291a066d74
uniqueusername <aryan@jha.io>
2021-04-05 16:21:11 -0700

    ternary operation for grammar

--- commands/tracker/count.js
+++ commands/tracker/count.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "count",
     description: "number of time keywords have been mentioned",
     execute(message, args) {
-        message.channel.send(`the timer has been reset \`\`${message.client.tracker.count}\`\` times`)
+        message.channel.send(`the timer has been reset \`\`${message.client.tracker.count}\`\` ${message.client.tracker.count == 1 ? "time" : "times"}`)
     }
 }
