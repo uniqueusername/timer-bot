879b6b050ef28fd34a12695d7c77acaadb7e7fb9
uniqueusername <aryan@jha.io>
2021-04-18 23:57:29 -0700

    additional info on last

--- commands/tracker/last.js
+++ commands/tracker/last.js
@@ -4,13 +4,13 @@ module.exports = {
     execute(message, args) {
         let elapsed = new Date() - new Date(message.client.tracker.date)
         if (elapsed < 60000) {
-            message.channel.send(`\`\`${(elapsed / 1000).toFixed(0)}\`\` seconds`)
+            message.channel.send(`\`\`${(elapsed / 1000).toFixed(0)}\`\` seconds since last trigger`)
         } else if (elapsed < 3600000) {
-            message.channel.send(`\`\`${Math.floor(elapsed / 60000)}\`\` minutes and \`\`${((elapsed % 60000) / 1000).toFixed(0)}\`\` seconds`)
+            message.channel.send(`\`\`${Math.floor(elapsed / 60000)}\`\` minutes and \`\`${((elapsed % 60000) / 1000).toFixed(0)}\`\` seconds since last trigger`)
         } else if (elapsed < 86400000) {
-            message.channel.send(`\`\`${Math.floor(elapsed / 3600000)}\`\` hours \`\`${Math.floor((elapsed % 3600000) / 60000)}\`\` minutes and \`\`${Math.floor((elapsed % 60000) / 1000).toFixed(0)}\`\` seconds`)
+            message.channel.send(`\`\`${Math.floor(elapsed / 3600000)}\`\` hours \`\`${Math.floor((elapsed % 3600000) / 60000)}\`\` minutes and \`\`${Math.floor((elapsed % 60000) / 1000).toFixed(0)}\`\` seconds since last trigger`)
         } else if (elapsed < 2073600000) {
-            message.channel.send(`\`\`${Math.floor(elapsed / 86400000)}\`\` days \`\`${Math.floor((elapsed % 86400000) / 3600000)}\`\` hours \`\`${Math.floor((elapsed % 3600000) / 60000)}\`\` minutes and \`\`${Math.floor((elapsed % 60000) / 1000)}\`\` seconds`)
+            message.channel.send(`\`\`${Math.floor(elapsed / 86400000)}\`\` days \`\`${Math.floor((elapsed % 86400000) / 3600000)}\`\` hours \`\`${Math.floor((elapsed % 3600000) / 60000)}\`\` minutes and \`\`${Math.floor((elapsed % 60000) / 1000)}\`\` seconds since last trigger`)
         }
     }
 }
